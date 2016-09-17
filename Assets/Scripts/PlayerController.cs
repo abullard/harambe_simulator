b67b9fb998c3848e4a75d165427c75873079fe07
Austin Bullard <austinbullard@protonmail.ch>
2016-09-17 08:42:07 -0500

    Harambe now picks up kids and shit

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,16 +5,19 @@ public class PlayerController : MonoBehaviour {
 
     public float speed;
     public bool isFlipped;
-    public Sprite grab;
     public Sprite hold;
 
     private int count;
     private bool carryingChild = false;
     private GameObject child;
+    private SpriteRenderer spRen;
+    private Sprite walk;
 
     public void Start() {
         count = 0;
         isFlipped = false;
+        spRen = GetComponent<SpriteRenderer>();
+        walk = spRen.sprite;
     }
 
     public void Update() {
@@ -37,6 +40,7 @@ public class PlayerController : MonoBehaviour {
             if (Input.GetKey(KeyCode.S))
             {
                 transform.position += Vector3.down * speed * Time.deltaTime;
+
             }
             if (Input.GetKey(KeyCode.D))
             {
@@ -52,7 +56,17 @@ public class PlayerController : MonoBehaviour {
             if (Input.GetKey(KeyCode.W))
             {
                 transform.position += Vector3.up * speed * Time.deltaTime;
-                child.transform.position += Vector3.up * speed * Time.deltaTime;
+                child.transform.position = transform.position;
+                if (!isFlipped)
+                {
+                    child.transform.position = transform.position;
+                    child.transform.position += new Vector3(.25f, -.1f);
+                }
+                else if (isFlipped)
+                {
+                    child.transform.position = transform.position;
+                    child.transform.position += new Vector3(-.19f, -.25f);
+                }
             }
             if (Input.GetKey(KeyCode.A))
             {
@@ -61,13 +75,32 @@ public class PlayerController : MonoBehaviour {
                     Flip();
                     isFlipped = true;
                 }
-                transform.position += Vector3.left * speed * Time.deltaTime;
-                child.transform.position += Vector3.left * speed * Time.deltaTime;
+                transform.position += Vector3.left * speed * Time.deltaTime;    
+                if (!isFlipped)
+                {
+                    child.transform.position = transform.position;
+                    child.transform.position += new Vector3(.25f, -.1f);
+                }
+                else if (isFlipped)
+                {
+                    child.transform.position = transform.position;
+                    child.transform.position += new Vector3(-.19f, -.25f);
+                }
             }
             if (Input.GetKey(KeyCode.S))
             {
                 transform.position += Vector3.down * speed * Time.deltaTime;
-                child.transform.position += Vector3.down * speed * Time.deltaTime;
+                child.transform.position = transform.position;
+                if (!isFlipped)
+                {
+                    child.transform.position = transform.position;
+                    child.transform.position += new Vector3(.25f, -.1f);
+                }
+                else if (isFlipped)
+                {
+                    child.transform.position = transform.position;
+                    child.transform.position += new Vector3(-.19f, -.25f);
+                }
             }
             if (Input.GetKey(KeyCode.D))
             {
@@ -77,12 +110,22 @@ public class PlayerController : MonoBehaviour {
                     isFlipped = false;
                 }
                 transform.position += Vector3.right * speed * Time.deltaTime;
-                child.transform.position += Vector3.right * speed * Time.deltaTime;
+                if (!isFlipped)
+                {
+                    child.transform.position = transform.position;
+                    child.transform.position += new Vector3(.25f, -.1f);
+                }
+                else if (isFlipped)
+                {
+                    child.transform.position = transform.position;
+                    child.transform.position += new Vector3(-.19f, -.25f);
+                }
+                
             }
         }
     } 
    
-    //Used to determine if Harambe is in contact with a child
+    //Used to determine if Harambe is in contact with a child or with his den
     void OnTriggerStay2D(Collider2D other)
     {
         if(other.gameObject.CompareTag("Child"))
@@ -91,16 +134,25 @@ public class PlayerController : MonoBehaviour {
                 child = other.gameObject;
                 child.transform.Rotate(Vector3.forward * 180);
                 child.transform.position = transform.position;
+                child.transform.localScale += new Vector3(-0.05f, -.05f, 0);
+                if(!isFlipped)
+                {
+                    child.transform.position += new Vector3(.25f, -.1f);
+                } else if(isFlipped) {
+                    child.transform.position += new Vector3(-.19f, -.25f);
+                }
+                
                 carryingChild = true;
+                spRen.sprite = hold;
             }
         }
         if(other.gameObject.CompareTag("Den_door")) {
-            if(carryingChild)
-            {
+            if(carryingChild) {
                 count++;
                 print(count);
                 Destroy(child);
                 carryingChild = false;
+                spRen.sprite = walk;
             }
         }
     }
