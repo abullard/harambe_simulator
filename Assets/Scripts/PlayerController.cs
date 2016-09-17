d1f30e66f57557c467090d786dc410c5756e2fb9
Austin Bullard <austinbullard@protonmail.ch>
2016-09-17 04:41:10 -0500

    Updated Colliders

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    public bool isFlipped;
+
+    public void Start()
+    {
+        isFlipped = false;
+    }
+
+    public void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.up * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            if (!isFlipped)
+            {
+                Flip();
+
+                isFlipped = true;
+            }
+
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.down * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            if (isFlipped)
+            {
+                Flip();
+
+                isFlipped = false;
+            }
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+    }
+
+    //Used to determine if Harambe is in contact with a child
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Child"))
+        {
+            
+        }
+    }
+
+    //Used to flip Harambe if another direction is pushed
+    public void Flip()
+    {
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
+}
