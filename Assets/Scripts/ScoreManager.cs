09336d79c47baa1f33fc9057b5b10f8f62bdcf07
Tyler Jaacks <TylerJaacks@Gmail.com>
2016-09-17 06:58:24 -0500

    Added Scoring System

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -2,8 +2,13 @@
 using System.Collections;
 
 public class ScoreManager : MonoBehaviour {
-    public int score;
+    public const int FINAL_SCORE_MODIFIER = 1000000;
+    public const int CURRENT_SCORE_MODIFIER = 1000;
+
+    public int currentScore;
+    public int finalScore;
     public int totalKids;
+    public int currentKids;
 
     public void Start()
     {
@@ -12,6 +17,21 @@ public class ScoreManager : MonoBehaviour {
 
     public void Update()
     {
-        
+        currentScore = (currentKids * CURRENT_SCORE_MODIFIER);
+    }
+
+    public void CalculateFinalScore()
+    {
+        finalScore = currentScore * FINAL_SCORE_MODIFIER;
+    }
+
+    public int GetCurrentScore()
+    {
+        return currentScore;
+    }
+
+    public int GetFinalScore()
+    {
+        return finalScore;
     }
 }
