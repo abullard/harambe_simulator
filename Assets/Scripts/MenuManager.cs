f2db7edab717c8d5994e36c50fa5f8a163d0a584
Tyler Jaacks <TylerJaacks@Gmail.com>
2016-09-16 22:52:19 -0500

    More Unity Shit

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour
+{
+	public void ChangeScene(string sceneName)
+	{
+		Application.LoadLevel(sceneName);
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
+}
